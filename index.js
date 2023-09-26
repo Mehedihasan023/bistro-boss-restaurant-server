0f368656b82f49d541cb1be09d3be172815e1ccc
Mehedihasan023 <mh20973@gmail.com>
2023-09-27 01:26:00 +0600

    jwt token install

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -60,7 +61,7 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
-        
+
         app.delete('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
