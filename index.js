44f96fdd741378a63a3233f69946b74ec370ae55
Mehedihasan023 <mh20973@gmail.com>
2023-09-29 01:59:01 +0600

    verify user to secure route for admin

--- index.js
+++ index.js
@@ -51,13 +51,23 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
 
             res.send({ token })
         })
+        // use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.admin !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next();
+        }
 
         //users related apis
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         });
@@ -72,6 +82,23 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result)
         });
+
+        // security layer: verifyJWT
+        //email same
+        //check admin
+
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
@@ -111,7 +138,7 @@ async function run() {
                 res.send([]);
             }
             const decodedEmail = req.decoded.email;
-            if(email !== decodedEmail){
+            if (email !== decodedEmail) {
                 return res.status(403).send({ error: true, message: 'forbidden access' });
             }
 
